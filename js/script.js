b182d0678881f0d0534bb1af8b09f8444f4e1619
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2022-09-04 21:20:25 +0430

    script file now has cleaner codes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
-let watch = document.getElementById("watch");
-let pmOrAm = document.querySelector(".timezone");
-let h;  let m;  let s;
+let watchEl = document.getElementById("watch"),
+    timeZone = document.querySelector(".timezone");
+let h,  
+    m,  
+    s;
 
 let showTime = setInterval(time,1000);
 
 function time(){
-        let getClock = new Date();
-
-        h = getClock.getHours();
-        m = getClock.getMinutes();
-        s = getClock.getSeconds();
+        let getTime = new Date();
+        h = getTime.getHours();
+        m = getTime.getMinutes();
+        s = getTime.getSeconds();
         if(h<12 && h>=0 ){
-            pmOrAm.innerText = "AM";
+            timeZone.innerText = "AM";
         }else{
             h=h-12;
-            pmOrAm.innerText = "PM";
+            timeZone.innerText = "PM";
         }
-        watch.innerText = `${h} : ${m} : ${s} `;
+        watchEl.innerText = `${h} : ${m} : ${s} `;
          if(h<10){
-             watch.innerText = `0${h} : ${m} : ${s} `;
+             watchEl.innerText = `0${h} : ${m} : ${s} `;
          }else if(m<10){
-             watch.innerText = `${h} : 0${m} : ${s} `;
+             watchEl.innerText = `${h} : 0${m} : ${s} `;
          }else if(s<10){
-             watch.innerText = `${h} : ${m} : 0${s} `;
+             watchEl.innerText = `${h} : ${m} : 0${s} `;
          }
 }
 
