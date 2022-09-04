28581c8f7bc385f632c60247b72fd900d61de59a
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2022-09-04 21:40:56 +0430

    bug fixed and responsive css added
    js file had a bug that didn't show 0 if the hour or second was less than 10

--- js/script.js
+++ js/script.js
@@ -17,13 +17,14 @@ function time(){
             h=h-12;
             timeZone.innerText = "PM";
         }
-        watchEl.innerText = `${h} : ${m} : ${s} `;
-         if(h<10){
-             watchEl.innerText = `0${h} : ${m} : ${s} `;
-         }else if(m<10){
-             watchEl.innerText = `${h} : 0${m} : ${s} `;
-         }else if(s<10){
-             watchEl.innerText = `${h} : ${m} : 0${s} `;
-         }
+        if( h < 10 ){
+            watchEl.innerText = `0${h} : ${m} : ${s} `;
+        }
+        if( m < 10 ){
+            watchEl.innerText = `${h} : 0${m} : ${s} `;
+        }
+        if( s < 10 ){
+            watchEl.innerText = `${h} : ${m} : 0${s} `;
+        }
 }
 
