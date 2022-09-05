d223a1244b5da944cb419d09cc07cec8231589c1
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2022-09-05 15:53:55 +0430

    cleaner codes in Script.js file
    ternary operator used instead of regular if

--- js/script.js
+++ js/script.js
@@ -7,44 +7,25 @@ let h,
 let showTime = setInterval(time,1000);
 
 function time(){
-        let getTime = new Date();
-        h = getTime.getHours();
-        m = getTime.getMinutes();
-        s = getTime.getSeconds();
-        if(h<12 && h>=0 ){
-            timeZone.innerText = "AM";
-        }else{
-            h=h-12;
-            timeZone.innerText = "PM";
-        }
-        if(document.body.clientWidth <= 599){
-            if( h < 10 )
-                h = `0${h}`;
-            else
-                h = h;
-            if( m < 10 )
-                m = `0${m}`;
-            else
-                m = m;
-            if( s < 10 )
-                s = `0${s}`;
-            else
-                s = s;
-            watchEl.innerHTML = `${h} <br> ${m} <br> ${s} `;
-        }else{
-            if( h < 10 )
-                h = `0${h}`;
-            else
-                h = h;
-            if( m < 10 )
-                m = `0${m}`;
-            else
-                m = m;
-            if( s < 10 )
-                s = `0${s}`;
-            else
-                s = s;
-            watchEl.innerText = `${h} : ${m} : ${s} `;
-        }
+    let getTime = new Date();
+    h = getTime.getHours();
+    m = getTime.getMinutes();
+    s = getTime.getSeconds();
+    if(h<12 && h>=0 ){
+        timeZone.innerText = "AM";
+    }else{
+        h=h-12;
+        timeZone.innerText = "PM";
+    }
+    if(document.body.clientWidth <= 599){
+        h = h < 10 ? h = `0${h}` : h = h;
+        m = m < 10 ? m = `0${m}` : m = m;
+        s = s < 10 ? s = `0${s}` : s = s;
+        watchEl.innerHTML = `${h} <br> ${m} <br> ${s} `;
+    }else{
+        h = h < 10 ? h = `0${h}` : h = h;
+        m = m < 10 ? m = `0${m}` : m = m;
+        s = s < 10 ? s = `0${s}` : s = s;
+        watchEl.innerText = `${h} : ${m} : ${s} `;
+    }
 }
-
